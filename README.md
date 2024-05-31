45461f766f7c977a97924cc25db1fa3c8c5bb013
Everett Lancaster <lancastermaster@outlook.com>
2024-05-31 12:08:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ToonyRoguelike
+
+Developed with Unreal Engine 5
---
+++ Source/ToonyRoguelike/ToonyRoguelike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ToonyRoguelike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToonyRoguelike, "ToonyRoguelike" );
